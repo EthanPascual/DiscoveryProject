8b39c879e99bd8ef7cc26cadd5e9e9f2892d574b
gameboy2003 <ethan.pascual@gmail.com>
2024-02-02 13:05:09 -0500

    Linked Statistics Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Homepage from "./components/Homepage";
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GameRoom from "./components/GameRoom";
+import Stats from "./components/Stats"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/gameroom" element={<GameRoom />} />
+        <Route path="/stats" element={<Stats />} />
       </Routes>
     </Router>
   );
