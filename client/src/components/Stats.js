f6dd58b66bb5f603cca153a43cfbf040244a265e
echen03-gif <edward.chen@stonybrook.edu>
2024-02-04 14:03:43 -0500

    Added Base Code for Statistics Page

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,8 +1,56 @@
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
+
 export default function Stats(props){
+    const [userList, setUserList] = useState([]);
+
+  
+    useEffect(() => {
+        axios.get('http://localhost:8000/users').then(res => {
+      
+      setUserList(res.data);
+
+    });
+    }, []);
+
+    console.log(userList);
 
     return(
         <>
         <h1>Statistics</h1>
+
+        <div>
+            Total Wins:
+            <ul>
+            {
+            [...userList].sort((a, b) => b.wins - a.wins).map((user) => (
+                <li key={user._id}>{user.name} - Wins: {user.wins}</li>
+            ))
+            }
+            </ul>
+        </div>
+
+        <div>
+            Total Losses:
+            <ul>
+            {
+            [...userList].sort((a, b) => b.losses - a.losses).map((user) => (
+                <li key={user._id}>{user.name} - Losses: {user.losses}</li>
+              ))
+            }
+            </ul>
+        </div>
+
+        <div>
+            Total Guesses:
+            <ul>
+            {
+            [...userList].sort((a, b) => b.totalGuess - a.totalGuess).map((user) => (
+                <li key={user._id}>{user.name} - Total Guesses: {user.totalGuess}</li>
+              ))
+            }
+            </ul>
+        </div>
         </>
     )
 }
